2be02e1e0f6a1a2a92b2efc4bc1580f20891d2e6
MattTitmas <90630353+MattTitmas@users.noreply.github.com>
2021-10-19 20:22:32 +0100

    Update Text recognition.py

--- Text recognition.py
+++ Text recognition.py
@@ -4,28 +4,28 @@ import sys
 
 
 class NeuralNetwork(object):
-    def __init__(Self):
+    def __init__(Self): # Constructor Field
         #784,512,256,128,10
-        Self.InputLayers = 784
+        Self.InputLayers = 784 # Number of pixels per number in the MNIST dataset
         Self.HiddenLayerOne = 512
-        Self.HiddenLayerTwo = 256
+        Self.HiddenLayerTwo = 256 # Slowly reduce the number of nodes per layer
         Self.HiddenLayerThree = 128
-        Self.OutputLayer = 10
+        Self.OutputLayer = 10 # Possible numbers 0-10
         Self.LearningRate = 0.4
 
-        Self.Weights1 = np.random.randn(Self.InputLayers,Self.HiddenLayerOne)
+        Self.Weights1 = np.random.randn(Self.InputLayers,Self.HiddenLayerOne) # Start by generating random weights (Side Effect: Every time you run the program you'll get different final weights)
         Self.Weights2 = np.random.randn(Self.HiddenLayerOne,Self.HiddenLayerTwo)
         Self.Weights3 = np.random.randn(Self.HiddenLayerTwo,Self.HiddenLayerThree)
         Self.Weights4 = np.random.randn(Self.HiddenLayerThree,Self.OutputLayer)
 
 
-        Self.HiddenOneBias = np.random.rand(1, Self.HiddenLayerOne)
+        Self.HiddenOneBias = np.random.rand(1, Self.HiddenLayerOne) # Randomly generate biases
         Self.HiddenTwoBias = np.random.rand(1, Self.HiddenLayerTwo)
         Self.HiddenThreeBias = np.random.rand(1, Self.HiddenLayerThree)
         Self.OutputBias = np.random.rand(1, Self.OutputLayer)
 
 
-    def ForwardPropogation(Self, Input):
+    def ForwardPropogation(Self, Input): # Propogate an input through the neural network to produce an output
         Self.DotProduct1 = np.dot(Input, Self.Weights1)
         Self.DotProduct1 += Self.HiddenOneBias
         Self.Activated1 = Self.Sigmoid(Self.DotProduct1)     # Hidden Layer One
@@ -40,7 +40,7 @@ class NeuralNetwork(object):
         Prediction = Self.Sigmoid(Self.DotProduct4)
         return Prediction                                     # Output Layer
 
-    def BackwardPropogation(Self, X, Y, Prediction):
+    def BackwardPropogation(Self, X, Y, Prediction): # Propogate a result backwords through the neural network using gradient descent to find the Error Delta of each weight
         Self.PredictionError = Y - Prediction
         Self.PredictionDelta = Self.PredictionError * Self.SigmoidPrime(Prediction)
 
@@ -65,18 +65,18 @@ class NeuralNetwork(object):
         Self.HiddenTwoBias += Self.HiddenTwoDelta
         Self.HiddenThreeBias += Self.HiddenThreeDelta
     
-    def Sigmoid(Self, S):
+    def Sigmoid(Self, S): # Sigmoid normalises values between 0 and 1
         return (1/(1+np.exp(-S)))
 
-    def SigmoidPrime(Self, S):
+    def SigmoidPrime(Self, S): # Derivation of sigmoid, used for back propogation
         return (S*(1-S))
 
-    def Train(Self,X,Y):
+    def Train(Self,X,Y): # Trains the neural network
         Prediction = Self.ForwardPropogation(X)
         Self.BackwardPropogation(X,Y,Prediction)
 
 
-    def MeanSquaredError(Self,Prediction, Actual):
+    def MeanSquaredError(Self,Prediction, Actual): # Calculate overall error of Neural Network
          return sum(sum((Prediction - Actual)**2)/len(Prediction))
 
 
@@ -89,7 +89,7 @@ TrainingFile.close()
 
 
 
-Labels = []
+Labels = [] # Ensuring data is easily readable for the program
 for x in range(1,42000):
     Temp = TrainingLines[x].split(",")
     Labels.append(Temp[0])
@@ -108,34 +108,9 @@ for x in range(len(Pixels)):
             Pixels[x][y] = 1
 
 
-'''
-Draw the numbers
-
-
-for x in range(0,100):
-    if int(Labels[x]) == 7:
-        Pixel = Pixels[x]
-        for x in range(0,28):
-            String = ""
-            for y in range(0,28):
-                if Pixel[y + x*28] != 0:
-                    String += "@"
-                else:
-                    String +=" "
-            print(String)
-        print("\n")
-
-'''
-'''
-np.random.seed(0)
-bot = NeuralNetwork()
-bot.Train(np.array([1,1,1]),[1])
-'''
-
-
 
 AI = NeuralNetwork()
-Epochs = 3
+Epochs = 3 # Can't be too high - risk overtraining the Neural Network
 #==================#
 # Training network #
 #==================#
@@ -165,7 +140,7 @@ for x in range(Epochs):
         State = np.reshape(Pixels[x],[1,784])
         AI.Train(State,Wanted)
         if x % 1000 == 0:
-            print(x)
+            print(x) # Can be commented out - allows the user to track how far into the dataset the program is
     
 np.save("AWeight1", AI.Weights1)
 np.save("AWeight2", AI.Weights2)
@@ -174,7 +149,7 @@ np.save("AWeight4", AI.Weights4)
 np.save("AHiddenOneBias", AI.HiddenOneBias)
 np.save("AHiddenTwoBias", AI.HiddenTwoBias)
 np.save("AHiddenThreeBias", AI.HiddenThreeBias)
-np.save("AOutputBias", AI.OutputBias)
+np.save("AOutputBias", AI.OutputBias) # Save weights as numpy files
 
 Percent = 0
 Total = 0
@@ -183,7 +158,7 @@ for x in range(100):
     if int(Labels[x]) == int(Prediction):
         Percent += 1
     Total += 1
-print((Percent/Total)*100)
-    
+print((Percent/Total)*100) # Prints the percent of the dataset that is guessed correctly TODO - download test set and use that instead
+   
 
 
